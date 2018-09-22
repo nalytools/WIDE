8b98697b4fdc8b931da13ec3211f74d0094d05c1
nataly.medina <nataly.medina@cetys.mx>
2018-09-22 12:50:22 -0700

    Config.yml

--- src/main/java/mx/cetys/tij/Main.java
+++ src/main/java/mx/cetys/tij/Main.java
@@ -7,7 +7,9 @@ public class Main {
     public static void main(String [] args)
     {
 
-        System.out.println(args[0]); // WGG - added line
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(args[i]); // WGG - added line
+        }
     }
 
 }
---
+++ src/test/java/mx/cetys/tij/MainTest.java
@@ -0,0 +1,12 @@
+package mx.cetys.tij;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        Main.main(new String[]{"arg1", "arg2"});
+        assertTrue(true);
+    }
+}
